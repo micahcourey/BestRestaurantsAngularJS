395164769a2bc8e99f1a8ae300e14cafdef512a4
micahcourey <micahjcourey@gmail.com>
2015-09-21 15:38:01 -0700

    add properties for cuisine, address, and price

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -1,11 +1,15 @@
 bestRestaurants.controller('RestaurantsCtrl', function RestaurantsCtrl($scope) {
   $scope.restaurants = [];
+  $scope.cuisine = [];
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({name: $scope.restaurantName})
+    $scope.restaurants.push({name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, address: $scope.restaurantAddress, price: $scope.restaurantPrice });
     $scope.restaurantName = null;
+    $scope.restaurantCuisine = null;
+    $scope.restaurantAddress = null;
+    $scope.restaurantPrice = null;
   };
   $scope.deleteRestaurant = function(restuarant) {
-    var index = $scope.restaurants.indexOf(restaurant)
+    var index = $scope.restaurants.indexOf(restaurant);
     $scope.restaurants.splice(index, 1);
   }
 });
