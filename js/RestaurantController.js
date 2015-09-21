2fb25805920c31a2cfa6093e4c048d9ced06e313
micahcourey <micahjcourey@gmail.com>
2015-09-21 14:34:14 -0700

    add html form

--- js/RestaurantController.js
+++ js/RestaurantController.js
@@ -1,1 +1,11 @@
-var bestRestaurants = angular.module('bestRestaurant', []);
+bestRestaurants.controller('RestaurantsCtrl', function StudentsCtrl($scope) {
+  $scope.restaurants = [];
+  $scope.addRestaurant = function() {
+    $scope.restaurants.push({name: $scope.restaurantName})
+    $scope.restaurantName = null;
+  };
+  $scope.deleteRestaurant = function(restuarant) {
+    var index = $scope.restaurants.indexOf(restaurant)
+    $scope.restaurants.splice(index, 1);
+  }
+});
